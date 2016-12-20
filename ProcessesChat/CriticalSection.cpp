22517879e9a70a523203787809b8cf8a1a55491b
alexey3nemckovich <alexey3nemckovich@gmail.com>
2016-12-20 10:03:09 +0300

    Critical section enter takes less CPU time.

--- ProcessesChat/CriticalSection.cpp
+++ ProcessesChat/CriticalSection.cpp
@@ -22,7 +22,10 @@ bool CriticalSection::TryEnter()
 
 void CriticalSection::Enter()
 {
-    while (!TryEnter());
+    while (!TryEnter())
+    {
+        std::this_thread::yield();
+    }
 }
 
 
--- ProcessesChat/CriticalSection.h
+++ ProcessesChat/CriticalSection.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <thread>
+using namespace std;
 
 
 class CriticalSection
